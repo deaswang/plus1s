66b31e35f1cd4fea92b7f880aac67995dadd626d
Frank Wang <deas.wang@gmail.com>
2018-08-21 22:46:21 +0800

    add basic func

--- forum/views.py
+++ forum/views.py
@@ -1,9 +1,11 @@
+
+from datetime import datetime, timedelta
 from django.shortcuts import render, get_object_or_404
 
 from .models import Thread, Post, Comment
 
 def index(request):
-    thread_list = Thread.objects.order_by('-life')[:10]
+    thread_list = Thread.objects.order_by('-life')
     context = {
         'thread_list': thread_list,
     }
@@ -12,6 +14,31 @@ def index(request):
 
 def thread(request, thread_id):
     thread = get_object_or_404(Thread, pk=thread_id)
-    return render(request, 'thread.html', {'thread': thread})
+    if request.method == 'POST':
+        name = request.POST['name']
+        content = request.POST['content']
+        add = Post(thread=thread, pub_name=name, pub_date=datetime.now(), content=content)
+        add.save()
+    try:
+        post_list = Post.objects.filter(thread=thread).order_by('pub_date')
+    except:
+        post_list = None
+    return render(request, 'thread.html', {'thread': thread, "post_list": post_list})
 
+
+def newthread(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        name = request.POST['name']
+        content = request.POST['content']
+        add = Thread(title=title, pub_name=name, pub_date=datetime.now(), content=content,
+                     life=timedelta(days=1))
+        add.save()
+        thread_list = Thread.objects.order_by('-life')[:10]
+        context = {
+            'thread_list': thread_list,
+        }
+        return render(request, 'index.html', context)
+    else:
+        return render(request, 'newthread.html', {})
 
